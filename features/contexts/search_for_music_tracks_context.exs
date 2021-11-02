a4ddd58473537e92cc8ea71ed6c5555a2d7b8ca7
aurore jeremie <aurore@MacBook-Pro-de-aurore.local>
2021-11-02 15:15:14 +0100

    Add main context

--- features/contexts/search_for_music_tracks_context.exs
+++ features/contexts/search_for_music_tracks_context.exs
@@ -1,5 +1,7 @@
 defmodule Contexts.SearchForMusicTracksContext do
   use WhiteBread.Context
 
-  #sub_contexts
+  import_steps_from Contexts.ListenerContext
+  import_steps_from Contexts.MusicLibraryContext
+  import_steps_from Contexts.TrackListContext
 end
